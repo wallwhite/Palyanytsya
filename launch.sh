700be751710c63618a042b1deac96119615a921d
rexvel <illia.romanenko@verygoodsecurity.com>
2022-03-06 23:23:23 +0200

    testing script on ubuntu server

--- launch.sh
+++ launch.sh
@@ -5,13 +5,16 @@ git clone https://github.com/wallwhite/Palyanytsya.git
 cd ./Palyanytsya
 
 UNAME=$(uname)
+bash ./scripts/init-linux.sh
 
-if [ "$UNAME" == "Linux" ] ; then
-  bash ./scripts/init-linux.sh
-elif [ "$UNAME" == "Darwin" ] ; then
-  bash ./scripts/init-mac.sh
-else  
-  bash ./scriptsinit-windows.sh
-fi
+# commented due to testing purposes
+
+# if [ "$UNAME" == "Linux" ] ; then
+#   bash ./scripts/init-linux.sh
+# elif [ "$UNAME" == "Darwin" ] ; then
+#   bash ./scripts/init-mac.sh
+# else  
+#   bash ./scriptsinit-windows.sh
+# fi
 
 bash start.sh
