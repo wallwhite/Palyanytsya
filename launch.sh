763586ab7bdd235afa29ece5d7f576ce82767a3d
rexvel <illia.romanenko@verygoodsecurity.com>
2022-03-06 23:31:42 +0200

    testing script on ubuntu server#1

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
-git clone https://github.com/wallwhite/Palyanytsya.git 
+git clone https://github.com/wallwhite/Palyanytsya.git
 
 cd ./Palyanytsya
 
 UNAME=$(uname)
-bash ./scripts/init-linux.sh
+echo $uname
 
 # commented due to testing purposes
 
@@ -13,8 +13,27 @@ bash ./scripts/init-linux.sh
 #   bash ./scripts/init-linux.sh
 # elif [ "$UNAME" == "Darwin" ] ; then
 #   bash ./scripts/init-mac.sh
-# else  
+# else
 #   bash ./scriptsinit-windows.sh
 # fi
+
+echo "initialization on Linux"
+
+echo "Updating packages..."
+sudo apt update
+
+# TODO add universal check function
+
+# init-linux.sh
+echo "Installing Node"
+sudo apt-get install -y nodejs
+
+
+echo "Install yarn "
+npm install --global yarn
+
+echo "Install Typescript"
+npm install -g typescript
+
 
 bash start.sh
